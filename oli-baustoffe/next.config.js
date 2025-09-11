91140e5bc9f203b92ed567cd84ac6c6fb529fe25
Aaron Engelmann <zac.upshure@outlook.com>
2025-09-11 22:08:05 +0200

    Update next.config.js lottie ausgeklammert um deployment error zu vermeiden

--- oli-baustoffe/next.config.js
+++ oli-baustoffe/next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   images: {
     domains: ['oli-baustoffe-images.s3.eu-central-1.amazonaws.com'],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({ 'lottie-web': 'commonjs lottie-web' });
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
