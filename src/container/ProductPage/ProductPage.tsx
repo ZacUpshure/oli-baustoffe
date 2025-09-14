e33978e5b18df90541c7abf2eb78f720a4ead0bf
Aaron Engelmann <zac.upshure@outlook.com>
2025-09-14 22:43:42 +0200

    Update import statement for Lottie in ProductPage.tsx

---
+++ src/container/ProductPage/ProductPage.tsx
@@ -0,0 +1,15 @@
+// Importing necessary modules
+import React from 'react';
+import Lottie from '@/components/LottieClient';
+// other imports...
+
+const ProductPage = () => {
+    // Component logic...
+    return (
+        <div>
+            {/* Component JSX */}
+        </div>
+    );
+};
+
+export default ProductPage;
