54ad8e0e18ddfbab29f07f9f236e03a02b7647c1
Aaron Engelmann <zac.upshure@outlook.com>
2025-09-14 22:43:50 +0200

    Updated import statement in Dachplatten.tsx

---
+++ src/container/Dachplatten/Dachplatten.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Lottie from "@/components/LottieClient";
+// other imports and code
+
+const Dachplatten = () => {
+    // component code
+};
+
+export default Dachplatten;
